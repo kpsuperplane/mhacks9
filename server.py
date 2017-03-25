62c4dd070be76958667c6c1baaa60059d7be19ac
Marcel O'Neil <marcel@marceloneil.com>
2017-03-25 02:14:52 -0400

    Speech Recognition

--- server.py
+++ server.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import speech_recognition as sr
+import configparser
 
 app = Flask(__name__, static_url_path='/public/build', template_folder='public/build')
+
+config = configparser.ConfigParser()
+config.read('secrets.cfg')
+upload_path = config['mhacks']['path']
+wit = config['mhacks']['wit']
+houndify_id = config['mhacks']['houndify_id']
+houndify_key = config['mhacks']['houndify_key']
+
+r = sr.Recognizer()
+r.energy_threshold = 4000
+r.dynamic_energy_threshold = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/audio', methods=['POST'])
+def audio():
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            return 'No file upload'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No file upload 2'
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(upload_path, filename)
+            file.save(filepath)
+            with sr.AudioFile(filepath) as source:
+                audio = r.record(source)
+                # print(r.recognize_wit(audio, wit))
+                return r.recognize_houndify(audio, houndify_id, houndify_key)
+        else:
+            return 'No file upload 3'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
