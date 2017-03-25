fa4f66dd8dc6dbe14dbe7724d181beb4019c4ba5
Marcel O'Neil <marcel@marceloneil.com>
2017-03-25 09:45:08 -0400

    Only Bing

--- server.py
+++ server.py
@@ -55,8 +55,8 @@ def audio():
                         "webm_path": domain + "/files/" + filename + ".webm",
                         "flac_path": domain + "/files/" + filename + ".flac",
                         "transcripts": {
-                            "wit": r.recognize_wit(audio, wit),
-                            "ibm": r.recognize_ibm(audio, ibm_username, ibm_password, language="en-US"),
+                            # "wit": r.recognize_wit(audio, wit),
+                            # "ibm": r.recognize_ibm(audio, ibm_username, ibm_password, language="en-US"),
                             "bing": r.recognize_bing(audio, microsoft, language="en-US") #,
                           # "houndify": r.recognize_houndify(audio, houndify_id, houndify_key) #,
                           # "google": r.recognize_google_cloud(audio, credentials_json=google_credentials, language="en-US")
