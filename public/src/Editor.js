0a2d03ab5615edfe5e1dc0f859f4a21e235ac9bd
Curtis Chong <curtischong5@gmail.com>
2017-03-26 09:12:53 -0400

    merge

--- public/src/Editor.js
+++ public/src/Editor.js
@@ -12,6 +12,7 @@ import 'react-quill/dist/quill.snow.css';
 import 'react-quill/dist/quill.core.css';
 import ChangeMode from './ChangeMode.js';
 import Record from "react-icons/lib/md/adjust";
+import ReactAudioPlayer from 'react-audio-player';
 
 class Editor extends Component {
   constructor(){
@@ -26,8 +27,8 @@ class Editor extends Component {
       theDeltas: [],
       recordingLength: 0
     }
-    this.video_segments = [[0,6,"213"],[6,9,"264"]];
-
+    this.audio_segments = [[0,6,"213"],[6,9,"264"]];
+    this.currentAudio = "";
     this.add_range = this.add_range.bind(this);
     this.shift_indexes = this.shift_indexes.bind(this);
 
@@ -52,15 +53,16 @@ class Editor extends Component {
       })
       ctx.recorder.ondataavailable = (e) => {
         request.post("https://mhacks.1lab.me/audio").field("file", e.data).end(function(err, res){
-          const editor = this.refs.editor.getEditor();
-          var hi = editor.getContents();
+          //const editor = ctx.refs.editor.getEditor();
+          //var hi = editor.getContents();
           var changes = ctx.state.theDeltas;
           for(var i = 0 ; i < changes.length; i ++){
-            ctx.add_range(changes[i][0][res.body.webm_path]);
+            ctx.add_range(changes[i][0], changes[i][1], [res.body.webm_path]);
+            //console.log(changes[i]);
           }
-          console.log(res.body.webm_path);
-          console.log(ctx.state.theDeltas);
-          console.log(this.refs.editor.getEditor().getContents());
+          ctx.currentAudio = res.body.webm_path;
+          console.log(ctx.audio_segments);
+          //console.log(this.refs.editor.getEditor().getContents());
         });
         new Audio(window.URL.createObjectURL(e.data)).play();
       }
@@ -142,54 +144,62 @@ class Editor extends Component {
   }
 
   shift_indexes(start_index, amount){
-    for(var i = start_index; i < this.video_segments.length; i++){
-      this.video_segments[i][0] += amount;
-      this.video_segments[i][1] += amount;
+    for(var i = start_index; i < this.audio_segments.length; i++){
+      this.audio_segments[i][0] += amount;
+      this.audio_segments[i][1] += amount;
     }
   }
 
   delete_range(first, last){
     var diff = last-first;
-    for(var i = 0 ; i < this.video_segments.length; i++){
-      var idx1 = this.video_segments[i][0];
-      var idx2 = this.video_segments[i][1];
+    for(var i = 0 ; i < this.audio_segments.length; i++){
+      var idx1 = this.audio_segments[i][0];
+      var idx2 = this.audio_segments[i][1];
       if(first >= idx1 && last < idx2){
         if(first === idx1){
           if(diff === idx2 - idx1){
             //this catches the case where the entire deletion makes up the entire segment
           }else{
-            this.video_segments[i][1] = idx2 - diff;
+            this.audio_segments[i][1] = idx2 - diff;
           }
         }else{
-          this.video_segments[i][1] = idx2 - diff;
+          this.audio_segments[i][1] = idx2 - diff;
           //delete the current range and proceed to shift everything left by n characters
         }
         this.shift_indexes(i+1, diff);
       }
     }
-    console.log(this.video_segments);
+    console.log(this.audio_segments);
   }
 
   add_range(first, last, video){
-    if(first === this.video_segments.length){//we are appending the new video clip to the end of the document
-      this.video_segments.push([first,last,video]);
+    console.log("adding_range");
+    console.log(first);
+    console.log(last);
+    //console.log(this.audio_segments.length);
+    last = 1;
+    const editor = this.refs.editor.getEditor();
+    var hi = editor.getContents();
+      console.log(hi.ops[0].insert.length)
+    if(first+2 == hi.ops[0].insert.length){//we are appending the new video clip to the end of the document
+      this.audio_segments.push([first,last,video]);
+      console.log("added");
     }else{
-      for(var i = 0 ; i < this.video_segments.length; i++){
-        var idx1 = this.video_segments[i][0];
-        var idx2 = this.video_segments[i][1];
+      console.log("added2");
+      for(var i = 0 ; i < this.audio_segments.length; i++){
+        var idx1 = this.audio_segments[i][0];
+        var idx2 = this.audio_segments[i][1];
         if(first >= idx1 && last < idx2){
-          //this shrinks the first range and then pushes two extra ranges to. (effectively a split)
-          this.video_segments[i][1] = first;
-          this.video_segments.push([first,last,video]);
-          this.video_segments.push([last,idx2 + 1,this.video_segments[i][2]]);
+          console.log("hi");
+            //this shrinks the first range and then pushes two extra ranges to. (effectively a split)
+          this.audio_segments[i][1] = first;
+          this.audio_segments.push([first,last,video]);
+          this.audio_segments.push([last,idx2 + 1,this.audio_segments[i][2]]);
         }
       }
     }
     this.shift_indexes(i+1, last-first);
   }
-
-
-
 
   onResize(){
     const toolbarContainer = document.getElementsByClassName('ql-toolbar')[0];
@@ -198,58 +208,6 @@ class Editor extends Component {
     editorContainer.style.padding = "15px " + Math.max(10, window.innerWidth/2 - 390) + "px";
     editorContainer.style.height = (window.innerHeight - editorContainer.getBoundingClientRect().top)+"px";
   }
-/*
-  var video_segments = [[0,6,"213"],[6,9,"264"]];
-
-  var shift_indexes = function(start_index, amount){
-    for(var i = start_index; i < video_segments.length; i++){
-      video_segments[i][0] += amount;
-      video_segments[i][1] += amount;
-    }
-  };
-
-
-  var delete_range = function(first, last){
-    var diff = last-first;
-    for(var i = 0 ; i < video_segments.length; i++){
-      var idx1 = video_segments[i][0];
-      var idx2 = video_segments[i][1];
-      if(first >= idx1 && last < idx2){
-        if(first == idx1){
-          if(diff == idx2 - idx1){
-            //this catches the case where the entire deletion makes up the entire segment
-          }else{
-            video_segments[i][1] = idx2 - diff;
-          }
-        }else{
-          video_segments[i][1] = idx2 - diff;
-          //delete the current range and proceed to shift everything left by n characters
-        }
-        shift_indexes(i+1, diff);
-      }
-    }
-    console.log(video_segments);
-  };
-
-
-  var add_range = function(first, last, video){
-    if(first == video_segments.length){//we are appending the new video clip to the end of the document
-      video_segments.push([first,last,video]);
-    }else{
-      for(var i = 0 ; i < video_segments.length; i++){
-        var idx1 = video_segments[i][0];
-        var idx2 = video_segments[i][1];
-        if(first >= idx1 && last < idx2){
-          //this shrinks the first range and then pushes two extra ranges to. (effectively a split)
-          video_segments[i][1] = first;
-          video_segments.push([first,last,video]);
-          video_segments.push([last,idx2 + 1,video_segments[i][2]]);
-        }
-      }
-    }
-  };*/
-
-
 
   render() {
     return (
@@ -257,6 +215,7 @@ class Editor extends Component {
         <Navbar>
           <ChangeMode changeState={this.changeState.bind(this)}/>
           <button><Record /> <span>{Math.floor(this.state.recordingLength/60)}:{(this.state.recordingLength%60 < 10 ? "0": "") + this.state.recordingLength%60}</span></button>
+          <ReactAudioPlayer src={this.currentAudio} autoPlay/>
         </Navbar>
         <ReactQuill ref="editor" onChangeSelection={this.onChangeSelection} onChange={this.onChange} placeholder="Type notes here..."  theme="snow" />
         <Highlight data={this.database} curIndex={this.state.curRecordIndex} editor={this.state.editor} />
