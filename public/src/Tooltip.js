e86cbdae7e274444552cd79920f1e255341d3b34
caroly <carolyfisher@gmail.com>
2017-03-26 02:44:07 -0400

    Everything for Wolfram but the update

--- public/src/Tooltip.js
+++ public/src/Tooltip.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Tooltip.css';
-import popup from "./popup.png";
 import request from "superagent";
 
 class App extends Component {
@@ -8,38 +7,42 @@ class App extends Component {
     wolframRequest(query, callback) {
       request.get("https://api.wolframalpha.com/v2/query")
         .query({
-          input: "food definition",
+          input: query + " definition",
           appid: "WPHVGV-P8YW5TGLQX",
           format: "image,plaintext",
           output: 'json'
         })
         .end(callback);
     }
-  render() {
+    var imageData = React.createClass({    
+    getData(data){
+      const ctx = this; 
+      this.wolframRequest(data, (err, result) => {
+        this.setState({data: JSON.parse(result.text).queryresult.pods[1].subpods[0].img.src}); 
+        console.log(JSON.parse(result.text).queryresult.pods[1].subpods[0].img.src);
+        return data; 
+      }.bind(this)); 
+    },
+    return (
+        <ImageData data={this.state.data} />
+        )
+    });
 
+  render() {
     const quillContents = document.getElementsByClassName('quill-contents');
     const offset = quillContents.length ? quillContents[0].getBoundingClientRect().top : 0;
     const containerLeft = Math.min(Math.max(this.props.position.x - 225, 10), window.outerWidth - 235);
-
-    this.wolframRequest(this.props.content, (err, result) => {
-      console.log(result.queryresult);
-    });
-
-
+    const data;
     return (
       <div className={"tooltip-container " + (this.props.content === null || "visible")}
         style={{top: this.props.position.y + offset + 2,
-                left: (this.props.position.x / window.outerWidth) * 100 + (window.outerWidth - this.props.width / 2) + "%", 
+                left: (this.props.position.x / window.outerWidth) * 200 + (window.outerWidth - this.props.width / 2) + "%", 
                 transformOrigin: ((this.props.position.x - containerLeft - 10)/this.props.position.x)+"% 0%"}}>
           <div className="tooltip-arrow" style={{
-            left: (this.props.position.x / window.outerWidth) * 100 + (window.outerWidth - this.props.width / 2) + "%"}}></div>
+            left: (this.props.position.x / window.outerWidth) * 200 + (window.outerWidth - this.props.width / 2) + 5 + "%"}}></div>
           <div className="tooltip">
             <h4 className="title">{this.props.content}</h4>
-            <img alt="Wolfram Alpha Search Result" src={popup} />
-            <script>
-
-
-            </script>
+            <img alt="Wolfram Alpha Search Result" src={this.getData(this.props.content)} />
         </div>
       </div>
     );
