2bb1684a506b53ab522015b102113fdb3e748817
Carol Chen <carolyfisher@gmail.com>
2017-10-19 15:25:01 -0500

    Oops haaha

--- public/src/ChangeMode.js
+++ public/src/ChangeMode.js
@@ -10,15 +10,15 @@ export default class ChangeMode extends Component {
   //this.context.state = "Study";
   handleClick() {
     if(this.state.btn === "Study"){
-      this.setState({btn: "Study"})
-    }else{
       this.setState({btn: "Edit"})
+    }else{
+      this.setState({btn: "Study"})
     }
     this.props.changeState();
   }
   render () {
     return (
-      <button type="submit" onClick={() => this.handleClick()} className="btn btn-primary">{this.state.btn}</button>
+      <button type="submit" onClick={() => this.handleClick()} className="btn btn-primary">{this.state.btn}ing</button>
     )
   }
 }
