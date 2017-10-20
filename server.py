0f876efbc78593744643ed1b112f4182b9324792
Marcel O'Neil <marcel@marceloneil.com>
2017-10-20 11:52:07 -0400

    fix server

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ houndify_key = config['mhacks']['houndify_key']
 ibm_username = config['mhacks']['ibm_username']
 ibm_password = config['mhacks']['ibm_password']
 microsoft = config['mhacks']['microsoft']
-google_credentials = open('google_credentials.json', 'r').read()
+# google_credentials = open('google_credentials.json', 'r').read()
 wolfram_appid = config['mhacks']['wolfram_appid']
 
 r = sr.Recognizer()
@@ -117,4 +117,4 @@ def audio():
         else:
             return 'No file'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(port=5001)
